2158e61dfc782830372d017d63fcb2fe0b92ef89
DumontGontran <dumont.gontran@orange.fr>
2023-02-06 12:47:15 +0100

    creation of a first angular app

---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,48 @@
+import { Component, Input } from '@angular/core';
+import { Hero } from '../hero';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { HeroService } from '../hero.service';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent {
+  
+  @Input() hero?: Hero;
+  searchTerms: any;
+  heroes$: any;
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {}
+  ngOnInit(): void {
+    this.getHero();
+  };
+  
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  };
+  goBack(): void {
+    this.location.back();
+  };
+  save(): void {
+    if (this.hero) {
+      this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
+    };
+  };
+  search(): void {
+    this.heroes$ = this.searchTerms.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((term: string) => this.heroService.searchHeroes(term)),
+  )
+  }
+};
